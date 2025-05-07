833e0522e4a666ca66b3277fba2b0ef86e46d98d
qoiol <hjjj1324@gmail.com>
2025-05-07 18:52:43 +0900

    redis 설정

---
+++ src/main/java/com/example/postservice/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.example.postservice.config;
+
+import com.example.postservice.model.dto.UserDTO;
+import io.lettuce.core.RedisURI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisURI redisURI = RedisURI.create(redisProperties.getUrl());
+        RedisConfiguration configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(configuration);
+        factory.afterPropertiesSet();
+        return factory;
+    }
+
+
+    @Bean
+    public RedisTemplate<String, UserDTO> userDTORedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, UserDTO> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<UserDTO>(UserDTO.class));
+
+        return redisTemplate;
+    }
+}
